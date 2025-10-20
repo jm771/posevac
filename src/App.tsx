926e7df3b2d68952f5afc233bf3d78cc598a9265
Developer <dev@interactive-graph-tool.local>
2025-10-20 14:55:59 -0400

    Reactifying step 1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function MyApp() {
+  return (
+    <div className="level-selector" id="levelSelector">
+        <div className="level-selector-content">
+            <h1>Graph Esolang</h1>
+            <p className="subtitle">Programming Challenges</p>
+            <div className="levels-grid" id="levelsGrid">
+            </div>
+        </div>
+    </div>
+  );
+}
