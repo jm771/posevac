08afa7c6058b62fe6716ced1bdea674e5e59e4ea
Developer <dev@interactive-graph-tool.local>
2025-10-27 17:49:55 -0400

    New condition type

---
+++ src/condition.ts
@@ -0,0 +1,44 @@
+enum Matcher {
+  Zero,
+  One,
+  Wild,
+}
+
+function matcherMatches(matcher: Matcher, value: unknown) {
+  switch (matcher) {
+    case Matcher.Wild:
+      return true;
+    case Matcher.One:
+      return value === 1;
+    case Matcher.Zero:
+      return value === 0;
+  }
+}
+
+export class Condition {
+  matchers: Matcher[];
+
+  constructor(matchers: Matcher[]) {
+    this.matchers = matchers;
+  }
+
+  matches(values: unknown | unknown[]) {
+    if (this.matchers.length === 0) {
+      return true;
+    }
+
+    if (values instanceof Array) {
+      if (values.length == this.matchers.length) {
+        return values.every((val, index) =>
+          matcherMatches(this.matchers[index], val)
+        );
+      }
+    } else {
+      if (this.matchers.length == 1) {
+        return matcherMatches(this.matchers[0], values);
+      }
+    }
+
+    return false;
+  }
+}
