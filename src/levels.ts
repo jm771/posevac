826b43c4e394d3d3e36bfa31a7392fe30943cba9
Developer <dev@interactive-graph-tool.local>
2025-10-28 15:03:43 -0400

    new levels!

--- src/levels.ts
+++ src/levels.ts
@@ -13,57 +13,76 @@ export interface Level {
   allowedNodes: ComponentType[];
 }
 
-export const LEVELS: Level[] = [
-  {
-    id: "addition",
-    name: "Addition",
-    description:
-      "Learn the basics! Connect two numbers to a plus node to add them together.",
-    testCases: [
-      {
-        inputs: [
-          [3, 5, 7],
-          [7, 8, 10],
-        ],
-        expectedOutputs: [[10, 13, 17]],
-      },
-      {
-        inputs: [
-          [1, 3, 5],
-          [2, 4, 6],
-        ],
-        expectedOutputs: [[3, 7, 11]],
-      },
-    ],
-    allowedNodes: ["plus"],
-  },
-  {
-    id: "cum-sum",
-    name: "Cumulative Sum",
-    description:
-      "Learn basic looping and branching - compute the cumulative sum of some numbers",
-    testCases: [
-      {
-        inputs: [[1, 2, 3, 4, 5]],
-        expectedOutputs: [[1, 3, 6, 10, 15]],
-      },
-    ],
-    allowedNodes: ["plus", "constant"],
-  },
-  {
-    id: "factorial",
-    name: "Factorial",
-    description:
-      "Split and combine together with conditional branches let us compute factorial",
-    testCases: [
-      {
-        inputs: [[4]],
-        expectedOutputs: [[24]],
-      },
-    ],
-    allowedNodes: ["split", "combine", "constant", "multiply", "plus"],
-  },
-];
+const ADDITION: Level = {
+  id: "addition",
+  name: "Addition",
+  description:
+    "Learn the basics! Connect two numbers to a plus node to add them together.",
+  testCases: [
+    {
+      inputs: [
+        [3, 5, 7],
+        [7, 8, 10],
+      ],
+      expectedOutputs: [[10, 13, 17]],
+    },
+    {
+      inputs: [
+        [1, 3, 5],
+        [2, 4, 6],
+      ],
+      expectedOutputs: [[3, 7, 11]],
+    },
+  ],
+  allowedNodes: ["plus"],
+};
+
+const CUM_SUM: Level = {
+  id: "cum-sum",
+  name: "Cumulative Sum",
+  description:
+    "Learn basic looping and branching - compute the cumulative sum of some numbers",
+  testCases: [
+    {
+      inputs: [[1, 2, 3, 4, 5]],
+      expectedOutputs: [[1, 3, 6, 10, 15]],
+    },
+  ],
+  allowedNodes: ["plus", "constant"],
+};
+
+const MULTIPLY: Level = {
+  id: "multiply",
+  name: "Multiply",
+  description: "Build the multiply component",
+  testCases: [
+    {
+      inputs: [[3], [5]],
+      expectedOutputs: [[15]],
+    },
+    {
+      inputs: [[2], [4]],
+      expectedOutputs: [[8]],
+    },
+  ],
+  allowedNodes: ["split", "combine", "constant", "plus"],
+};
+
+const FACTORIAL: Level = {
+  id: "factorial",
+  name: "Factorial",
+  description:
+    "Split and combine together with conditional branches let us compute factorial",
+  testCases: [
+    {
+      inputs: [[4]],
+      expectedOutputs: [[24]],
+    },
+  ],
+  allowedNodes: ["split", "combine", "constant", "multiply", "plus"],
+};
+
+export const LEVELS: Level[] = [ADDITION, CUM_SUM, MULTIPLY, FACTORIAL];
 
 export function getLevelById(id: string): Level {
   const ret = LEVELS.find((level) => level.id === id);
